bbcf1b5a716513eaac2d2880bd89c80ab7d1e937
jwlh00 <juunshuu00@gmail.com>
2023-08-02 08:42:44 -0600

    Initial Functions

---
+++ XMPP/Data/IService/IXmppService.cs
@@ -0,0 +1,8 @@
+﻿namespace XMPP.Data
+{
+	public interface IXmppService
+	{
+		void SendMessage(string toJid, string message);
+		void Connect(string username, string password);
+	}
+}
---
+++ XMPP/Data/Service/XmppService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Matrix;
+using Matrix.Xmpp;
+using Matrix.Xmpp.Client;
+
+public class XmppClientService
+{
+	private readonly XmppClient _xmppClient;
+	private readonly string _username;
+	private readonly string _password;
+
+	public XmppClientService(string xmppDomain = "alumchat.xyz")
+	{
+		_username = "junwoo20358";
+		_password = "NOmeabandonepls123!@#";
+
+		_xmppClient = new XmppClient
+		{
+			XmppDomain = xmppDomain,
+			Username = _username,
+			Password = _password,
+			Resource = "MatriX"
+		};
+
+	}
+
+	public async Task ConnectAsync()
+	{
+		await _xmppClient.ConnectAsync();
+
+		_xmppClient.SendAsync(new Presence(Show.Chat));
+	}
+
+	public async Task SendMessageAsync(string to, string messageBody)
+	{
+		var msg = new Message(new Jid(to), MessageType.Chat, messageBody);
+		await _xmppClient.SendAsync(msg);
+	}
+
+}
